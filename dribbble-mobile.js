9af89fd3522dffd69270df2f927783cfcf1a4ddb
junib <eujeni@list.ru>
2017-03-01 20:40:05 +0300

    Update dribbble-mobile.js

--- dribbble-mobile.js
+++ dribbble-mobile.js
@@ -3,11 +3,18 @@
 // @namespace https://dribbble.com
 // @description (Enjoy) dribbble without js
 // @include https://dribbble.com/*
-// @version 0.1
+// @version 0.2
 // @author junib
 // ==/UserScript==
 
 (() => {
+    
+    var imgs = document.querySelectorAll('.dribbble-shot .dribbble-img .dribbble-link picture img');
+
+    if (imgs.length) {
+        imgs.forEach(el => el.src.replace(/_teaser(\..+)$/, "_1x$1"));
+    }
+    
     let st = document.createElement('style');
     st.innerHTML = 'body.grid-small ol.dribbbles li.group[id^="screenshot"], ol.dribbbles li.group div.dribbble-img img { width: 100% }';
     document.head.appendChild(st);
