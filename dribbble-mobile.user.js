6fa61450fcb2230f2fbff7ad95e4bab89dc23e73
junib <eujeni@list.ru>
2017-06-12 11:31:01 +0300

    Made some enhancements

--- dribbble-mobile.user.js
+++ dribbble-mobile.user.js
@@ -1,29 +1,46 @@
 // ==UserScript==
 // @name Dribbble Max & Better Img mobile
 // @namespace https://dribbble.com
-// @description (Enjoy) dribbble without js
+// @description (Enjoy) dribbble with(out) js
 // @include https://dribbble.com/*
-// @version 0.2.2
+// @version 0.3.0
 // @author junib
 // ==/UserScript==
 
 (() => {
     
-    var imgs = document.querySelectorAll('.dribbble-shot .dribbble-img .dribbble-link picture');
-
-    if (imgs.length) {
-        imgs.forEach(el => {
-            let img = document.createElement('img');
-            img.src = el.firstElementChild.srcset;
-
-            let par = el.parentNode;
-            par.insertBefore(img, el);
-
-            par.removeChild(el);
-        });
-    }
-    
-    let st = document.createElement('style');
-    st.innerHTML = 'body.grid-small ol.dribbbles li.group[id^="screenshot"], ol.dribbbles li.group div.dribbble-img img { width: 100% }';
+    var st = document.createElement('style');
+    st.innerHTML = '#main ol.dribbbles { max-width: none }' +
+        '#main ol.dribbbles li.group { width: 100%; margin: 0 }' +
+        'ol.dribbbles li.group div.dribbble-img img { width: 100% }' +
+        '#wrap-inner { padding: 0 }';
     document.head.appendChild(st);
+
+    let changeImg = (el) => {   
+            let pic = el.querySelector('.dribbble-link picture');
+            let picsrc = pic.firstElementChild.srcset.replace(/_1x(\..+)$/, "$1");
+            let img = document.createElement('img');
+            img.src = picsrc;
+
+            let par = pic.parentNode;
+            par.insertBefore(img, pic);
+
+            // removing
+            par.removeChild(pic);
+            // removing overlay. why???
+            par.parentNode.removeChild(par.nextElementSibling);
+    };
+
+    // first call
+    let litems = document.querySelectorAll('.dribbbles > .group');
+    litems.forEach((litem) => changeImg(litem) );
+
+    // waiting for new dribbbles to come
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            mutation.addedNodes.forEach((litem) => changeImg(litem) );
+        });
+    });
+    observer.observe(document.querySelector('.dribbbles'), { childList: true });
+
 })();
