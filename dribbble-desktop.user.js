b7309020734de933fa8e959791e1e6c0a3578aac
Eugene B <eujeni@list.ru>
2017-11-01 12:47:16 +0300

    For Dribbble's sake

--- dribbble-desktop.user.js
+++ dribbble-desktop.user.js
@@ -3,23 +3,61 @@
 // @namespace   https://dribbble.com
 // @description Load large images for dribbble and delete overlays with(out) JS
 // @include     https://dribbble.com/*
-// @version     0.3.1
+// @version     0.4.0
 // @author      junib
 // @grant       none
 // ==/UserScript==
 
 (() => {
+    
+    if (jQuery) {
+        var list = [
+            { name: 'RECENT', link: '/shots?sort=recent&list=attachments' },
+            { name: 'Landing', link: '/tags/landing' },
+            { name: 'Landing Page', link: '/tags/landing_page' },
+            { name: 'Website', link: '/tags/website' },
+            { name: 'Website Design', link: '/tags/website_design' },
+            { name: 'Webdesign', link: '/tags/webdesign' }
+        ];
+        
+        var sublist = [
+            { name: 'Fashion', link: '/tags/fashion' },
+            { name: 'Furniture', link: '/tags/furniture' },
+            { name: 'Ecommerce', link: '/tags/ecommerce' },
+            { name: 'Jewelry', link: '/tags/jewelry' },
+            { name: 'Jewellery', link: '/tags/jewellery' },
+            { name: 'Artist', link: '/tags/artist' },
+            { name: 'Agency', link: '/tags/agency' },
+            { name: 'Designer', link: '/tags/designer' },
+            { name: 'Lawyer', link: '/tags/lawyer' },
+            { name: 'Portfolio', link: '/tags/portfolio' }
+        ]
+        
+        var $ = jQuery;
+        var subul = $('<ul class="tabs"></ul>');
+        sublist.forEach(li => {
+            subul.append(`<li><a href="${li.link}">${li.name}</a></li>`);
+        });
+        
+        var ul = $('#header').append('<div class="group"><ul id="nav"></ul></div>').children().last().find('#nav');
+        list.forEach(el => {
+            ul.append(`<li><a href="${el.link}">${el.name}</a></li>`);
+        });
+        
+        ul.append(`<li><a href="#" class="has-sub">More</a>${subul[0].outerHTML}</li>`);
+    }
+    
     window.onkeydown = scrollPage;
     
     function scrollPage(e) {
-        if (e.keyCode === 98) { // numPad 2 / scroll down
+        if ([83, 98].includes(e.keyCode)) { // numPad 2 or s / scroll down
             let shot = document.querySelector('[id^="screenshot-"]');
             if (!window.pageYOffset) {
                 shot.scrollIntoView();
             } else {
                 window.scrollBy(0, shot.clientHeight);
             }
-        } else if (e.keyCode === 101) { // numPad 5 / scroll up 
+        } else if ([87, 101].includes(e.keyCode)) { // numPad 5 or w / scroll up 
             let shot = document.querySelector('[id^="screenshot-"]');
             window.scrollBy(0, -shot.clientHeight);
         }
@@ -28,7 +66,7 @@
     var st = document.createElement('style');
     st.innerHTML = '#main ol.dribbbles { max-width: none }' +
         '#main ol.dribbbles li.group { width: 50%; margin: 0 }' +
-        'ol.dribbbles li.group div.dribbble-img img { width: 100% }';
+        'ol.dribbbles li.group div.dribbble-img img { width: 100%; max-width: 1320px; }';
     document.head.appendChild(st);
     
     let changeImg = (el) => {   
